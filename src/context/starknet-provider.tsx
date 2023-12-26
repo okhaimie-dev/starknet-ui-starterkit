7e1e5eda6c8e21c41bbbace2bbb29fe90ebef206
Tony Stark <oktonystark8@gmail.com>
2023-12-26 12:10:52 -0600

    feat: added starknet react

---
+++ src/context/starknet-provider.tsx
@@ -0,0 +1,34 @@
+"use client";
+import {ReactNode} from "react";
+
+import { sepolia, mainnet } from "@starknet-react/chains";
+import {
+	StarknetConfig,
+	publicProvider,
+	argent,
+	braavos,
+	useInjectedConnectors,
+	voyager,
+} from "@starknet-react/core";
+
+export function StarknetProvider({ children }: { children: ReactNode }) {
+	const { connectors } = useInjectedConnectors({
+		// Show these connectors if the user has no connector installed.
+		recommended: [argent(), braavos()],
+		// Hide recommended connectors if the user has any connector installed.
+		includeRecommended: "onlyIfNoConnectors",
+		// Randomize the order of the connectors.
+		order: "random",
+	});
+
+	return (
+		<StarknetConfig
+			chains={[mainnet, sepolia]}
+			provider={publicProvider()}
+			connectors={connectors}
+			explorer={voyager}
+		>
+			{children}
+		</StarknetConfig>
+	);
+}
